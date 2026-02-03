69d153ce27ec6b2ea2c2c54f1323a83b01abf674
maya <kirznermaya@gmail.com>
2026-02-03 14:36:41 +0200

    Добавили Home и Profile в меню

--- app/src/main/java/activities/MenuActivity.java
+++ app/src/main/java/activities/MenuActivity.java
@@ -1,5 +1,4 @@
 package activities;
-// jygjughkidt
 
 import android.os.Bundle;
 
@@ -17,15 +16,18 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import com.example.mayakirzner.HomeFragment;
 import com.example.mayakirzner.MainActivity;
+import com.example.mayakirzner.ProfileFragment;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
 import com.example.mayakirzner.R;
 
 public class MenuActivity extends AppCompatActivity {
-    // mhyfgdj hdfndxy,ktc f fjlxy
+
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     @Override
@@ -48,12 +50,20 @@ public class MenuActivity extends AppCompatActivity {
 
         NavigationView navigationView = findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.nav_main) {
+            if (item.getItemId() == R.id.nav_home) {
+                showFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.nav_local_game) {
                 startActivity(new Intent(this, MainActivity.class));
+            } else if (item.getItemId() == R.id.nav_profile) {
+                showFragment(new ProfileFragment());
             }
             drawerLayout.closeDrawer(GravityCompat.START);
             return true;
         });
+        if (savedInstanceState == null) {
+            showFragment(new HomeFragment());
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -61,5 +71,11 @@ public class MenuActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    private void showFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_container, fragment)
+                .commit();
     }
 }
