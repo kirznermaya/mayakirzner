fc14db78278ad3c2b3c5b7e43648c94f0ee3bf42
maya <kirznermaya@gmail.com>
2026-02-03 15:05:25 +0200

    Экран стал интегрированым, вся логика игры в TicTacToeModel Activity - посредник между UI и моделью

--- app/src/main/java/com/example/mayakirzner/MainActivity.java
+++ app/src/main/java/com/example/mayakirzner/MainActivity.java
@@ -2,24 +2,60 @@
 package com.example.mayakirzner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mayakirzner.R;
+import com.example.mayakirzner.models.TicTacToeModel;
 
 public class MainActivity extends AppCompatActivity {
+    private TicTacToeModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        model = new TicTacToeModel();
     }
+    public void onCellClick(View view) {
+              Button button = (Button) view;
+              String tag = button.getTag().toString();
+              String[] position = tag.split(",");
+              int row = Integer.parseInt(position[0]);
+              int col = Integer.parseInt(position[1]);
+
+              if (model.isLegal(row, col)) {
+                  model.makeMove(row, col);
+                  button.setText(model.getCurrentPlayer());
+
+                  if (model.checkWin()) {
+                      model.changePlayer();
+                      Toast.makeText(this, "Player " + model.getCurrentPlayer() + " wins!", Toast.LENGTH_SHORT).show();
+                      model.resetGame();
+                      resetBoard();
+                  } else if (model.isTie()) {
+                      Toast.makeText(this, "It's a tie!", Toast.LENGTH_SHORT).show();
+                      model.resetGame();
+                      resetBoard();
+                  } else {
+                      model.changePlayer();
+                  }
+              }
+           }
+
+           private void resetBoard() {
+                int[] buttonIds = {
+                        R.id.button00, R.id.button01, R.id.button02,
+                        R.id.button10, R.id.button11, R.id.button12,
+                        R.id.button20, R.id.button21, R.id.button22
+                };
+
+                for (int id : buttonIds) {
+                    Button button = findViewById(id);
+                    button.setText("");
+                }
+            }
 }
